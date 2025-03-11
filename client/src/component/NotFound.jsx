cfa2add1caa311f0663f6dadb0486fcc48a75a69
devYavya <yavya.sharma21@gmail.com>
2025-03-11 10:04:40 +0530

    NotFound

---
+++ client/src/component/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      <img src='./notfound.jpg' alt="404" height="600px" width="479px" />
+    </div>
+  )
+}
+
+export default NotFound;
+
+
